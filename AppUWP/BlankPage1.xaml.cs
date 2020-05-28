cadba78ff23d04a053ac1a5ad2fa393910d01883
Fay Wang <v-faywan@microsoft.com>
2020-05-28 13:38:51 +0800

    Add project files.

---
+++ AppUWP/BlankPage1.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Media.Capture.Frames;
+using Windows.Media.Core;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AppUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+        }
+
+        private Rectangle rectangle1;
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            rectangle1 = new Rectangle();
+            rectangle1.Fill = new SolidColorBrush(Windows.UI.Colors.Blue);
+            rectangle1.Width = 200;
+            rectangle1.Height = 100;
+            rectangle1.Stroke = new SolidColorBrush(Windows.UI.Colors.Black);
+            rectangle1.StrokeThickness = 3;
+            rectangle1.RadiusX = 50;
+            rectangle1.RadiusY = 10;
+
+            // When you create a XAML element in code, you have to add
+            // it to the XAML visual tree. This example assumes you have
+            // a panel named 'layoutRoot' in your XAML file, like this:
+            // <Grid x:Name="layoutRoot>
+            layoutRoot.Children.Add(rectangle1);
+
+            await PlayLiveVideo();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Compositor compositor = ElementCompositionPreview.GetElementVisual(rectangle1).Compositor;
+            var shadowVisual = compositor.CreateSpriteVisual();
+            Vector2 newSize = new Vector2(0, 0);
+            if (rectangle1 is FrameworkElement element)
+            {
+                newSize = new Vector2((float)element.ActualWidth, (float)element.ActualHeight);
+            }
+            shadowVisual.Size = newSize;
+
+            var dropShadow = compositor.CreateDropShadow();
+            dropShadow.BlurRadius = 10;
+            dropShadow.Opacity = 0.3f;
+            dropShadow.Offset = new Vector3(10,10,10);
+            dropShadow.Color = Windows.UI.Colors.Black;
+
+            shadowVisual.Shadow = dropShadow;
+
+            // Sets a Visual as child of the Control’s visual tree.
+            ElementCompositionPreview.SetElementChildVisual(rectangle1, shadowVisual);
+        }
+
+        private async Task PlayLiveVideo()
+        {
+            var allGroups = await MediaFrameSourceGroup.FindAllAsync();
+            var eligibleGroups = allGroups.Select(g => new
+            {
+                Group = g,
+
+                // For each source kind, find the source which offers that kind of media frame,
+                // or null if there is no such source.
+                SourceInfos = new MediaFrameSourceInfo[]
+                {
+        g.SourceInfos.FirstOrDefault(info => info.DeviceInformation?.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Front
+            && info.SourceKind == MediaFrameSourceKind.Color),
+        g.SourceInfos.FirstOrDefault(info => info.DeviceInformation?.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Back
+            && info.SourceKind == MediaFrameSourceKind.Color)
+                }
+            }).Where(g => g.SourceInfos.Any(info => info != null)).ToList();
+
+            if (eligibleGroups.Count == 0)
+            {
+                System.Diagnostics.Debug.WriteLine("No source group with front and back-facing camera found.");
+                return;
+            }
+
+            var selectedGroupIndex = 0; // Select the first eligible group
+            MediaFrameSourceGroup selectedGroup = eligibleGroups[selectedGroupIndex].Group;
+            MediaFrameSourceInfo frontSourceInfo = selectedGroup.SourceInfos[0];
+
+            MediaCapture mediaCapture = new MediaCapture();
+            MediaCaptureInitializationSettings settings = new MediaCaptureInitializationSettings()
+            {
+                SourceGroup = selectedGroup,
+                SharingMode = MediaCaptureSharingMode.ExclusiveControl,
+                MemoryPreference = MediaCaptureMemoryPreference.Cpu,
+                StreamingCaptureMode = StreamingCaptureMode.Video,
+            };
+            try
+            {
+                await mediaCapture.InitializeAsync(settings);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("MediaCapture initialization failed: " + ex.Message);
+                return;
+            }
+            var frameMediaSource1 = MediaSource.CreateFromMediaFrameSource(mediaCapture.FrameSources[frontSourceInfo.Id]);
+            VideoStreamer.SetPlaybackSource(frameMediaSource1);
+            VideoStreamer.Play();
+        }
+    }
+}
---
+++ AppUWP/MainPage.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AppUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<MyListViewItemsClass> Items { get; set; }
+
+        private List<string> ItemsFromModel;
+
+        private int _myFontSize;
+        public int MyFontSize
+        {
+            get { return _myFontSize; }
+            set
+            {
+                if (value != _myFontSize)
+                {
+                    _myFontSize = value;
+                    OnPropertyChanged("MyFontSize");
+                    // Font size changed, need to refresh ListView items
+                    LoadRefreshMyListViewItems();
+                }
+            }
+        }
+
+        public MainViewModel()
+        {
+            // set default font size
+            _myFontSize = 20;
+
+            // example data
+            ItemsFromModel = new List<string>()
+        {
+            "ABC",
+            "ABCDEF",
+            "ABCDEFGHI",
+        };
+
+            LoadRefreshMyListViewItems();
+        }
+
+        public void LoadRefreshMyListViewItems()
+        {
+            int itemMaxTextLength = 0;
+            foreach (var modelItem in ItemsFromModel)
+            {
+                if (modelItem.Length > itemMaxTextLength) { itemMaxTextLength = modelItem.Length; }
+            }
+            Items = new ObservableCollection<MyListViewItemsClass>();
+            // Convert points to pixels, multiply by max character length to determine fixed textblock width
+            // This assumes 96 DPI. Search for how to grab system DPI in C# there are answers on SO. 
+            double width = MyFontSize * 0.75 * itemMaxTextLength;
+            foreach (var itemFromModel in ItemsFromModel)
+            {
+                var item = new MyListViewItemsClass();
+                item.MyText = itemFromModel;
+                item.ItemFontSize = MyFontSize;
+                item.TextWidth = width;
+                Items.Add(item);
+            }
+            OnPropertyChanged("Items");
+        }
+
+        public class MyListViewItemsClass
+        {
+            public string MyText { get; set; }
+            public int ItemFontSize { get; set; }
+            public double TextWidth { get; set; }
+        }
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, e);
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+
+    public class GridViewCustomPanel : Panel
+    {
+        private double maxWidth;
+        private double maxHeight;
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var x = 0.0;
+            var y = 0.0;
+            foreach (var child in Children)
+            {
+                var newpos = new Rect(x, y, maxWidth, maxHeight);
+                child.Arrange(newpos);
+                y += maxHeight;
+            }
+            return finalSize;
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            foreach (var child in Children)
+            {
+                child.Measure(availableSize);
+
+                var desirtedwidth = child.DesiredSize.Width;
+                if (desirtedwidth > maxWidth)
+                    maxWidth = desirtedwidth;
+
+                var desiredheight = child.DesiredSize.Height;
+                if (desiredheight > maxHeight)
+                    maxHeight = desiredheight;
+            }
+            var itemperrow = Math.Floor(availableSize.Width / maxWidth);
+            var rows = Math.Ceiling(Children.Count / itemperrow);
+            return new Size(itemperrow * maxWidth, maxHeight * rows);
+        }
+    }
+
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var lv = new List<string>
+            {
+                "1", "22", "33", "444", "999999999"
+            };
+            lview.ItemsSource = lv;
+            //int itemMaxTextLength = 0;
+            //foreach (var modelItem in lv)
+            //{
+            //    if (modelItem.Length > itemMaxTextLength)
+            //    {
+            //        itemMaxTextLength = modelItem.Length;
+            //    }
+            //}
+
+            mainViewModel = new MainViewModel();
+            //double width = 14 * 0.75 * itemMaxTextLength;
+            //lview.Width = width;
+        }
+
+        private MainViewModel mainViewModel { get; set; }
+    }
+}
